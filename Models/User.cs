f46b48aa86a2dea0d56d80b45920f10f9a4dc97b
guilherme rodrigues <gui_rrl@hotmail.com>
2026-06-02 10:29:15 -0300

    Comecei a implementar a parte de autenticação e usuarios

---
+++ Models/User.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace RankingDigi.Models
+{
+    public class User : IdentityUser
+    {
+        // Chave estrangeira para o jogador (Player)
+        public int? PlayerId { get; set; }
+        public Player Player { get; set; }
+    }
+}
